509975a7f4b4c4556152a210ed4024bfa2c16636
elba3184 <elbachimilio@gmail.com>
2019-08-05 14:34:42 -0400

    Update-2
    Changed arrPhoneNum values

--- PreWork Challenges/createPhoneNum.js
+++ PreWork Challenges/createPhoneNum.js
@@ -31,5 +31,5 @@ function createPhoneNumber(numbers){
     
 }
 
-let arrPhoneNum = [7, 8, 6, 2, 3, 8, 4, 3, 0, 0];
+let arrPhoneNum = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 console.log(createPhoneNumber(arrPhoneNum)); //calls createPhoneNumber function that takes in an array of ints
