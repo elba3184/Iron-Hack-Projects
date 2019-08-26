f50d04b1ff4afb6c27325238ba4c8299bda7df9f
elba3184 <elbachimilio@gmail.com>
2019-08-26 17:45:41 -0400

    Rudimentary version 1.01

--- Labs/APIs/Project1/javascript/api.js
+++ Labs/APIs/Project1/javascript/api.js
@@ -1,6 +1,22 @@
+$(document).ready(() => {
+    document.getElementById('myCarousel').onclick = function () {
+      alert("Under Construction");
+    } 
+    document.getElementById('myCarousel').onclick = function () {
+        alert("Under Construction");
+    }
+    
+})
 axios.get("https://api.spacexdata.com/v3/launches/latest").then((result) => {
     console.log(result)
 }).catch((err) => {
     console.log(err)
 });
 
+// window.axios.get("https://api.spacexdata.com/v3/info").then((result) => {
+//     console.log(result)
+// }).catch((err) => {
+//     console.log(err)
+// });
+
+// addEventListener() 
--- Labs/APIs/Project1/javascript/index.js
+++ Labs/APIs/Project1/javascript/index.js
@@ -1,1 +1,8 @@
-$('.carousel').carousel()
+// $('.carousel').carousel()
+
+// const charactersAPI = new APIHandler("https://api.spacexdata.com/v3")
+
+// $(document).ready(() => {
+//     document.getElementById('fetch-all').onclick = function () {
+      
+//     })
