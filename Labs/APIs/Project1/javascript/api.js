57e0dfe698c98ac57a0b9bb885997c1cd5cc0225
elba3184 <elbachimilio@gmail.com>
2019-08-27 10:37:07 -0400

    Added 1-2-3-LiftOFF

--- Labs/APIs/Project1/javascript/api.js
+++ Labs/APIs/Project1/javascript/api.js
@@ -5,6 +5,9 @@ $(document).ready(() => {
     document.getElementById('myCarousel').onclick = function () {
         alert("Under Construction");
     }
+    document.getElementById('').onclick = function () {
+        alert("Elon Musk wants to know your location");
+      } 
     
 })
 axios.get("https://api.spacexdata.com/v3/launches/latest").then((result) => {
--- Labs/APIs/Project1/javascript/index.js
+++ Labs/APIs/Project1/javascript/index.js
@@ -1,4 +1,81 @@
-// $('.carousel').carousel()
+var ml4 = {};
+ml4.opacityIn = [0,1];
+ml4.scaleIn = [0.2, 1];
+ml4.scaleOut = 3;
+ml4.durationIn = 800;
+ml4.durationOut = 600;
+ml4.delay = 500;
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml4 .letters-1',
+    opacity: ml4.opacityIn,
+    scale: ml4.scaleIn,
+    duration: ml4.durationIn
+  }).add({
+    targets: '.ml4 .letters-1',
+    opacity: 0,
+    scale: ml4.scaleOut,
+    duration: ml4.durationOut,
+    easing: "easeInExpo",
+    delay: ml4.delay
+  }).add({
+    targets: '.ml4 .letters-2',
+    opacity: ml4.opacityIn,
+    scale: ml4.scaleIn,
+    duration: ml4.durationIn
+  }).add({
+    targets: '.ml4 .letters-2',
+    opacity: 0,
+    scale: ml4.scaleOut,
+    duration: ml4.durationOut,
+    easing: "easeInExpo",
+    delay: ml4.delay
+  }).add({
+    targets: '.ml4 .letters-3',
+    opacity: ml4.opacityIn,
+    scale: ml4.scaleIn,
+    duration: ml4.durationIn
+  }).add({
+    targets: '.ml4 .letters-3',
+    opacity: 0,
+    scale: ml4.scaleOut,
+    duration: ml4.durationOut,
+    easing: "easeInExpo",
+    delay: ml4.delay
+  }).add({
+    targets: '.ml4 .letters-4',
+    opacity: ml4.opacityIn,
+    scale: ml4.scaleIn,
+    duration: ml4.durationIn
+  }).add({
+    targets: '.ml4 .letters-4',
+    opacity: 0,
+    scale: ml4.scaleOut,
+    duration: ml4.durationOut,
+    easing: "easeInExpo",
+    delay: ml4.delay
+  }).add({
+    targets: '.ml4',
+    opacity: 0,
+    duration: 600,
+    delay: 450
+  });
+
+
+  setTimeout(()=>{
+    document.getElementById('go').remove()
+  },7500)
+
+ $(document).ready(() => {
+    document.getElementById('myCarousel').onclick = function () {
+      alert("Under Construction");
+    } 
+    document.getElementsByClassName('mars').onclick = function () {
+        alert("Elon Musk wants to know your location");
+      } 
+    
+})
 
 // const charactersAPI = new APIHandler("https://api.spacexdata.com/v3")
 
