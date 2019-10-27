178c610cebc213a264aa6758158f117a2c4b5907
Elba Chimilio <elbachimilio@gmail.com>
2019-10-27 15:08:08 -0400

    Finished binaries.js Coding Challenge

--- Coding Challenges/binaries.js
+++ Coding Challenges/binaries.js
@@ -11,3 +11,23 @@ function code(str) {
     }
     return currentNum;
 }
+
+function decode(str) {
+    let count = 0;
+    let temp = "";
+    let currentNum ="";
+    for (let i =0 ; i < str.length; i++) {
+      count ++;
+      if(Number(str[i]) !== 0) {
+        i++;
+        for(let j = 0; j < count; j++) {
+          temp += str[i+j];
+        }
+        currentNum += String(parseInt(temp,2));
+        temp = "";
+        i += count-1;
+        count =0;
+      }
+    }
+    return currentNum;
+}
