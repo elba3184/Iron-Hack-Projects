cf5338e0d0814712b0a19d0a86be03348f2da9e5
Elba Chimilio <elbachimilio@gmail.com>
2019-11-04 17:05:19 -0500

    Added leftRotation.js; need to finish completing

---
+++ Coding Challenges/leftRotation.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+  inputString += inputStdin;
+});
+
+process.stdin.on('end', function () {
+  inputString = inputString.replace(/\s*$/, '')
+    .split('\n')
+    .map(str => str.replace(/\s*$/, ''));
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+// Complete the rotLeft function below.
+function rotLeft(a, d) {
+  let actualRotations = k % n;
+  let newArray = [n];
+  let currentIndex = 0;
+
+  for (let i = actualRotations; i < n; i++) {
+    newArray[currentIndex] = a[i];
+    currentIndex++;
+  }
+
+  for (let i = 0; i < actualRotations; i++) {
+    newArray[currentIndex] = a[i];
+    currentIndex++;
+  }
+
+  return newArray;
+
+}
+
+function main() {
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+  const nd = readLine().split(' ');
+
+  const n = parseInt(nd[0], 10);
+
+  const d = parseInt(nd[1], 10);
+
+  const a = readLine().split(' ').map(aTemp => parseInt(aTemp, 10));
+
+  const result = rotLeft(a, d);
+
+  ws.write(result.join(' ') + '\n');
+
+  ws.end();
+}
