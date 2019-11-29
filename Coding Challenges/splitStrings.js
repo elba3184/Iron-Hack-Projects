39cfe3a0b847cabe96ef9d2da6a924dff86fae79
Elba Chimilio <elbachimilio@gmail.com>
2019-11-29 00:25:07 -0500

    Finished splitString.js; need to add notes

--- Coding Challenges/splitStrings.js
+++ Coding Challenges/splitStrings.js
@@ -1,9 +1,21 @@
 function solution(str){
-   var currArr = string.split("");
-    for (let i = 0 ; i < currArr.length; i = i+2) {
-      let pairedArr = [];
-      pairedArr.push(currArr[i], currArr[i+1])
-    }
+  var strArr = str.split("");
+	var arr1 = [];
+	var arr2 = [];
+	for (let i = 0 ; i < strArr.length; i = i+2) {
+		let pairedArr = [];
+		pairedArr.push(strArr[i], strArr[i+1]);
+		arr1.push(pairedArr);
+	}
+	for (let i = 0; i < arr1.length; i++) {
+	  let pairString = ""
+	  pairString = arr1[i].join("")
+	  arr2.push(pairString)
+	}
+	if (arr2[arr2.length-1].length%2 !== 0) {
+	  arr2[arr2.length-1]+= "_";
+	}
+	return arr2;
   
   }
   
